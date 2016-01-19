efbbdee2d5e1bafc4327238cb86b167e0174827c
Maksym Bevza <maksymbevza@gmail.com>
2016-01-19 15:31:27 +0200

    Moving downloading out

--- src/main/java/me/lab/crawler/Downloader.java
+++ src/main/java/me/lab/crawler/Downloader.java
@@ -7,9 +7,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Downloader {
     private static final int tries = 5;
+    private static final String site = "http://m.football.ua";
 
     public static void main(String[] args) throws IOException {
         File dir = new File(".");
@@ -19,16 +22,43 @@ public class Downloader {
         out.newLine();
         out.close();
 
-        //processPage("http://dt.ua");
-        processPage("http://m.football.ua");
+        processPage(site);
+    }
 
-        //File file = new File(loc);
+    private static String download(URL url) {
+        InputStream is = null;
+        BufferedReader br;
+        String line;
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            is = url.openStream();  // throws an IOException
+            br = new BufferedReader(new InputStreamReader(is));
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 
     public static void processPage(String URL) throws IOException {
         // invalid link
         if (URL.contains(".pdf") || URL.contains("@")
-                || URL.contains(":80")|| URL.contains(".jpg")
+                || URL.contains(":80") || URL.contains(".jpg")
                 || URL.contains(".pdf") || URL.contains(".jpg")) {
             return;
         }
@@ -46,11 +76,10 @@ public class Downloader {
             try {
                 Database.saveURL(URL, "");
                 for (int i = 0; doc == null && i < tries; ++i) {
-                    try {
-                        doc = Jsoup.connect(URL).get();
-                    } catch (HttpStatusException e) {
-                        System.err.println(e.getMessage());
-                    }
+                    //doc = Jsoup.connect(URL).timeout(3000).get();
+                    String html = download(new URL(URL));
+                    doc = Jsoup.parse(html);
+
                 }
             } catch (Exception e) {
                 e.printStackTrace();
